018b765a273704c5242f99d207e9c4a6295404cb
Bekzhan Satarov <satarovbekzhan@gmail.com>
2023-07-23 02:26:52 +0600

    quality of astronaut pic decreased to 50

--- src/app/[lang]/(base)/page.uc.tsx
+++ src/app/[lang]/(base)/page.uc.tsx
@@ -142,8 +142,8 @@ export default function Home({
           <Image
             src={astronautPic}
             alt={"Astronaut"}
-            // objectFit={"contain"}
             loading={"lazy"}
+            quality={50}
           />
         </Box>
       </Box>
